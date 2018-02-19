ec8cc0c1728f105efe3cc97c4760bd15751f0015
mynavathithavamaniramas <30829075+mynavathithavamaniramas@users.noreply.github.com>
2018-02-19 16:09:10 +0530

    Update myna30.c

--- myna30.c
+++ myna30.c
@@ -1,22 +1,11 @@
 #include<stdio.h>
 int main()
 {
-int a,i,sum=0,add=0;
-printf("enter value\t");
-scanf("%d",&a);
-for(i=1;i<=15;i++)
-{
-add+=i;
-}
-printf("THE SUM OF NUMBERS BETWEEN 1 TO 15 IS %d\n\t",add);
-while(a<=45)
-{
-if(a%3==0)
-{
-sum+=a;
-}
-a++;
-}
-printf("The sum of odd numbers between 15 and 45: %d",sum);
-return 0;
+float hour,minute,c;
+  printf("enter the two values"):
+  scanf("%f%f",&hour,&minute);
+  {
+    c=hour-minute;
+    printf("%d",c);
+  }
 }
